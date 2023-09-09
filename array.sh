142edca54c4c6ec17983bf8569facc4bb041391b
mannamsarath <mannamsarath58@gmail.com>
2023-09-09 20:41:58 +0530

    hey script

---
+++ array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This is called as array, instead of single value it can hold multiple values
+PERSONS=("Ramesh" "Suresh" "Sachin")
+#Inside array we always refer values with index and it starts from 0.
+#If we want first person
+echo "First Person: ${PERSONS[0]}"
