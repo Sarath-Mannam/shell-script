e4199484fb98b1d1e60d96d8765252794a628b91
mannamsarath <mannamsarath58@gmail.com>
2023-09-09 20:47:32 +0530

    hey script

--- array.sh
+++ array.sh
@@ -5,3 +5,5 @@ PERSONS=("Ramesh" "Suresh" "Sachin")
 #Inside array we always refer values with index and it starts from 0.
 #If we want first person
 echo "First Person: ${PERSONS[0]}"
+
+echo "First Person: ${PERSONS[@]}"
