6dac1261ea7b7c453e87212d80fcc745e6800abb
mannamsarath <mannamsarath58@gmail.com>
2023-09-09 20:01:09 +0530

    hey script

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,9 @@ echo "Please enter your username"
 read USERNAME #this is the variable where the value of username get stores
 
 echo "Username entered is $USERNAME"
+
+echo "Please enter your password"
+
+read PASSWORD
+
+echo "Password entered is $PASSWORD"
