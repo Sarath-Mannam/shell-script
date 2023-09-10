761678dfae174a35dba894c1fbb9e3917df41337
mannamsarath <mannamsarath58@gmail.com>
2023-09-10 12:50:57 +0530

    conditions in shell scripting

--- 08-install-git.sh
+++ 08-install-git.sh
@@ -7,6 +7,7 @@ USERID=$(id -u) # First to get the user id
 if [ $USERID -ne 0 ]  #-ne means not equal to 
 then 
     echo "ERROR:: Please run this script with root access"
+    exit 1 # if it finds exit other than 0, then it will come out of flow immediately 
 fi
 
 yum install git -y
