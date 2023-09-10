dad4d1f22c00dd1bde6c82de4b4bd354749887cc
mannamsarath <mannamsarath58@gmail.com>
2023-09-10 22:47:44 +0530

    conditions in shell scripting

--- 09-functions.sh
+++ 09-functions.sh
@@ -6,14 +6,18 @@ SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log 
 # Now i want to append all the output to this log file 
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 #This function should validate the previous command and inform user it is success or failure
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
-     echo " $2 ... Failure"
+     echo " $2 ... $R Failure $N"
      exit 1
 else
-     echo " $2 ... Success"
+     echo " $2 ... $G Success $N"
 fi
 } 
 
