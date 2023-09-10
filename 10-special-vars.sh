9cbf46b9907651d8d9fe5745224f04a3d053f391
mannamsarath <mannamsarath58@gmail.com>
2023-09-10 20:27:40 +0530

    conditions in shell scripting

--- 10-special-vars.sh
+++ 10-special-vars.sh
@@ -6,3 +6,8 @@ VAR2=$2
 echo "variable 1: $VAR1"
 
 echo "all vars: $@"
+
+ # i want to understand how many variables i 've passed
+echo "number of vars: $#" # here # - means how many number of variables passed.
+
+# so these are called special variables.
