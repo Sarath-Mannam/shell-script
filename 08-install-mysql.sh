1cda541ea293e489d627084a00f4e469f4174500
mannamsarath <mannamsarath58@gmail.com>
2023-09-10 13:33:26 +0530

    conditions in shell scripting

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -10,5 +10,15 @@ then
     exit 1 # if it finds exit other than 0, then it will come out of flow immediately 
 fi
 
-yum install mysql -y
+yum install mysqll -y
+
+if [ $? -ne 0]
+then 
+     echo "Installation of mysql is failed"
+     exit 1
+else 
+     echo "Installation of mysql is success"
+fi
+
+
 
