d2bafd5ccd3eed877d8225526867a744f4acf599
mannamsarath <mannamsarath58@gmail.com>
2023-09-12 09:07:53 +0530

    delete old log files

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -13,7 +13,7 @@ echo "$FILES_TO_DELETE"
 #here we reading the input line by line and deleting it.
 while read line
 do
-    echo "Deleting $line" &> $LOGFILE #storing logs inside logfile because client has to know what are the files deleted
+    echo "Deleting $line" &>> $LOGFILE #storing logs inside logfile because client has to know what are the files deleted
     rm -rf $line
 done <<< $FILES_TO_DELETE  # < less than symbol indicates input of file FILES_TO_DELETE
 # > Symbol is mening for output
