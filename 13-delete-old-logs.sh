d7fb37b77c00a4f2dc978e9bf7558bca0a5a6410
mannamsarath <mannamsarath58@gmail.com>
2023-09-12 08:40:37 +0530

    delete old log files

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -9,3 +9,11 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +10)
 
 echo "$FILES_TO_DELETE"
+
+#here we reading the input line by line and deleting it.
+while read line
+do
+    echo "Deleting $line" &> $LOGFILE #storing logs inside logfile because client has to know what are the files deleted
+    rm -rf $line
+done < $FILES_TO_DELETE  # < less than symbol indicates input of file FILES_TO_DELETE
+# > Symbol is mening for output
