2a69faf2c79653f5b0410d6e143c371687a7d690
mannamsarath <mannamsarath58@gmail.com>
2023-09-15 10:28:59 +0530

    added script to check the usge of disk

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+
 APP_LOGS_DIR=/home/centos/app-logs
+
 DATE=$(date +%F:%H:%M:%S)
+
 #If your manager or client asks what are the files you deleted last week, so then we have to show shell script log files
 LOGSDIR=/home/centos/shellscript-logs
+
 SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
+LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +10)
 
-echo "Script started executing at $DATE"
+echo "Script started executing at $DATE" &>>$LOGFILE
 
 #here we reading the input line by line and deleting it.
 while read line
 do
-    echo "Deleting $line" &>> $LOGFILE #storing logs inside logfile because client has to know what are the files deleted
+    echo "Deleting $line" &>>$LOGFILE #storing logs inside logfile because client has to know what are the files deleted
     rm -rf $line
 done <<< $FILES_TO_DELETE  # < less than symbol indicates input of file FILES_TO_DELETE
 # > Symbol is mening for output
